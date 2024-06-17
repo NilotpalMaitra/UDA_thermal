2a3b04d3e83247c40da759fe1083294d8b7c46ae
Nilvalox <nilotpalm2015@gmail.com>
2024-06-17 16:18:06 +0530

    commit

--- models/model.py
+++ models/model.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 from models.functions import ReverseLayerF
 import torch
 from torchvision import models
+import math
 import torch.nn.functional as F
 
 class BasicConv(nn.Module):
@@ -9,7 +10,7 @@ class BasicConv(nn.Module):
         super(BasicConv, self).__init__()
         self.out_channels = out_planes
         self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
-        self.bn = nn.BatchNorm2d(out_planes, eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
         self.relu = nn.ReLU() if relu else None
 
     def forward(self, x):
@@ -33,31 +34,31 @@ class ChannelGate(nn.Module):
             nn.Linear(gate_channels, gate_channels // reduction_ratio),
             nn.ReLU(),
             nn.Linear(gate_channels // reduction_ratio, gate_channels)
-        )
+            )
         self.pool_types = pool_types
-
     def forward(self, x):
         channel_att_sum = None
         for pool_type in self.pool_types:
-            if pool_type == 'avg':
-                avg_pool = F.avg_pool2d(x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-                channel_att_raw = self.mlp(avg_pool)
-            elif pool_type == 'max':
-                max_pool = F.max_pool2d(x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-                channel_att_raw = self.mlp(max_pool)
-            elif pool_type == 'lp':
-                lp_pool = F.lp_pool2d(x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-                channel_att_raw = self.mlp(lp_pool)
-            elif pool_type == 'lse':
+            if pool_type=='avg':
+                avg_pool = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( avg_pool )
+            elif pool_type=='max':
+                max_pool = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( max_pool )
+            elif pool_type=='lp':
+                lp_pool = F.lp_pool2d( x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp( lp_pool )
+            elif pool_type=='lse':
+                # LSE pool only
                 lse_pool = logsumexp_2d(x)
-                channel_att_raw = self.mlp(lse_pool)
+                channel_att_raw = self.mlp( lse_pool )
 
             if channel_att_sum is None:
                 channel_att_sum = channel_att_raw
             else:
-                channel_att_sum += channel_att_raw
+                channel_att_sum = channel_att_sum + channel_att_raw
 
-        scale = torch.sigmoid(channel_att_sum).unsqueeze(2).unsqueeze(3).expand_as(x)
+        scale = F.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
         return x * scale
 
 def logsumexp_2d(tensor):
@@ -68,116 +69,124 @@ def logsumexp_2d(tensor):
 
 class ChannelPool(nn.Module):
     def forward(self, x):
-        return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
+        return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1)), dim=1 )
 
 class SpatialGate(nn.Module):
     def __init__(self):
         super(SpatialGate, self).__init__()
         kernel_size = 7
         self.compress = ChannelPool()
-        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size - 1) // 2, relu=False)
-
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size-1) // 2, relu=False)
     def forward(self, x):
         x_compress = self.compress(x)
         x_out = self.spatial(x_compress)
-        scale = torch.sigmoid(x_out)
+        scale = F.sigmoid(x_out) # broadcasting
         return x * scale
 
 class CBAM(nn.Module):
     def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'lse'], no_spatial=True):
         super(CBAM, self).__init__()
         self.ChannelGate = ChannelGate(gate_channels, reduction_ratio, pool_types)
-        self.no_spatial = no_spatial
-        if not no_spatial:
-            self.SpatialGate = SpatialGate()
-
+        self.no_spatial=no_spatial
+        #if not no_spatial:
+            #self.SpatialGate = SpatialGate()
     def forward(self, x):
         x_out = self.ChannelGate(x)
-        if not self.no_spatial:
-            x_out = self.SpatialGate(x_out)
+        #if not self.no_spatial:
+            #x_out = self.SpatialGate(x_out)
         return x_out
+
+
+
 
 class CNNModel(nn.Module):
 
     def __init__(self):
         super(CNNModel, self).__init__()
         
-        pre_a = models.alexnet(pretrained=True)
+        pre_a =  models.alexnet(pretrained=True)
         self.features = pre_a.features[0:8]
-        for param in self.features.parameters():
-            param.requires_grad = False
+        for i,p in enumerate(self.features.parameters()):
+            #if(i<6): #[changed to [2,4,6] depending on how many conv layers to freeze], 132 for mobilenet
+            p.requires_grad = False
 
-        self.bn1 = nn.BatchNorm2d(128)
-        self.bn2 = nn.BatchNorm2d(64)
+        
+        self.bn1 = nn.BatchNorm2d(128) #128 for digit 256 for alpha
+        self.bn2 = nn.BatchNorm2d(64) # 64 for digit 128 for alpha
         self.relu = nn.ReLU(True)
 
-        self.cbam1 = CBAM(128)
-        self.cbam2 = CBAM(64)
+        self.cbam1 = CBAM(128) #128 for digit 256 for alpha
+        self.cbam2 = CBAM(64) # 64 for digit 128 for alpha
 
-        self.bottleneck2 = nn.Sequential(
-            nn.Conv2d(128, 32, kernel_size=1, stride=1, padding='same'),
-            nn.BatchNorm2d(32),
-            nn.ReLU(True),
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding='same', dilation=2),
-            nn.BatchNorm2d(32),
-            nn.ReLU(True),
-            nn.Conv2d(32, 128, kernel_size=1, stride=1, padding='same'),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True)
-        )
+        self.bottleneck2 = nn.Sequential()
+        self.bottleneck2.add_module('b2_conv1',nn.Conv2d(128, 32, kernel_size=1,stride=1,padding = 'same')) # change with 128 and 32 for digit, 256 and 64 for alpha
+        #self.bottleneck2.add_module('b2_r1',nn.ReLU(True))
+        self.bottleneck2.add_module('b2_bn1',nn.BatchNorm2d(32))
+        self.bottleneck2.add_module('b2_r1',nn.ReLU(True))
+        self.bottleneck2.add_module('b2_conv2',nn.Conv2d(32, 32, kernel_size=3,stride=1,padding = 'same',dilation=2))
+        #self.bottleneck2.add_module('b2_r2',nn.ReLU(True))
+        self.bottleneck2.add_module('b2_bn2',nn.BatchNorm2d(32))
+        self.bottleneck2.add_module('b2_r2',nn.ReLU(True))
+        self.bottleneck2.add_module('b2_conv3',nn.Conv2d(32, 128, kernel_size=1,stride=1,padding = 'same'))
+        #self.bottleneck2.add_module('b2_r3',nn.ReLU(True))
+        self.bottleneck2.add_module('b2_bn3',nn.BatchNorm2d(128))
+        self.bottleneck2.add_module('b2_r3',nn.ReLU(True))
+        
+        self.bottleneck4 = nn.Sequential()
+        self.bottleneck4.add_module('b4_conv1',nn.Conv2d(64, 16, kernel_size=1,stride=1,padding = 'same')) # change with 64 and 16 for digit, 128 and 32 for alpha
+        #self.bottleneck4.add_module('b4_r1',nn.ReLU(True))
+        self.bottleneck4.add_module('b4_bn1',nn.BatchNorm2d(16))
+        self.bottleneck4.add_module('b4_r1',nn.ReLU(True))
+        self.bottleneck4.add_module('b4_conv2',nn.Conv2d(16, 16, kernel_size=3,stride=1,padding = 'same', dilation=2))
+        #self.bottleneck4.add_module('b4_r2',nn.ReLU(True))
+        self.bottleneck4.add_module('b4_bn2',nn.BatchNorm2d(16))
+        self.bottleneck4.add_module('b4_r2',nn.ReLU(True))
+        self.bottleneck4.add_module('b4_conv3',nn.Conv2d(16, 64, kernel_size=1,stride=1,padding = 'same'))
+        #self.bottleneck4.add_module('b4_r3',nn.ReLU(True))
+        self.bottleneck4.add_module('b4_bn3',nn.BatchNorm2d(64))
+        self.bottleneck4.add_module('b4_r3',nn.ReLU(True))
+        #'''
+        
+        self.class_classifier = nn.Sequential()
+        self.class_classifier.add_module('c_fc1', nn.Linear(256, 100))  #256 for digit 128*4*4 for aplha
+        self.class_classifier.add_module('c_bn1', nn.BatchNorm1d(100)) # 500 for alpha
+        self.class_classifier.add_module('c_relu1', nn.ReLU(True))
+        #self.class_classifier.add_module('c_drop1', nn.Dropout2d())
+        self.class_classifier.add_module('c_fc2', nn.Linear(100, 100))
+        self.class_classifier.add_module('c_bn2', nn.BatchNorm1d(100))
+        self.class_classifier.add_module('c_relu2', nn.ReLU(True))
+        self.class_classifier.add_module('c_fc3', nn.Linear(100, 10)) #10 for digit and 4 for alpha
+        self.class_classifier.add_module('c_softmax', nn.LogSoftmax())
 
-        self.bottleneck4 = nn.Sequential(
-            nn.Conv2d(64, 16, kernel_size=1, stride=1, padding='same'),
-            nn.BatchNorm2d(16),
-            nn.ReLU(True),
-            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding='same', dilation=2),
-            nn.BatchNorm2d(16),
-            nn.ReLU(True),
-            nn.Conv2d(16, 64, kernel_size=1, stride=1, padding='same'),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True)
-        )
-
-        self.class_classifier = nn.Sequential(
-            nn.Linear(128 * 6 * 6, 100),  # Adjusted input size
-            nn.BatchNorm1d(100),
-            nn.ReLU(True),
-            nn.Linear(100, 100),
-            nn.BatchNorm1d(100),
-            nn.ReLU(True),
-            nn.Linear(100, 10),
-            nn.LogSoftmax(dim=1)
-        )
-
-        self.domain_classifier = nn.Sequential(
-            nn.Linear(128 * 6 * 6, 100),  # Adjusted input size
-            nn.BatchNorm1d(100),
-            nn.ReLU(True),
-            nn.Linear(100, 2),
-            nn.LogSoftmax(dim=1)
-        )
-
-        self.conv4 = nn.Conv2d(256, 128, kernel_size=3, dilation=2)
-        self.conv5 = nn.Conv2d(128, 64, kernel_size=3, dilation=2)
-        self.max3 = nn.MaxPool2d(kernel_size=3, stride=2)
-
+        self.domain_classifier = nn.Sequential()
+        self.domain_classifier.add_module('d_fc1', nn.Linear(128*4*4, 100))#256 for digit 128*4*4 for aplha
+        self.domain_classifier.add_module('d_bn1', nn.BatchNorm1d(100))
+        self.domain_classifier.add_module('d_relu1', nn.ReLU(True))
+        self.domain_classifier.add_module('d_fc2', nn.Linear(100, 2))
+        self.domain_classifier.add_module('d_softmax', nn.LogSoftmax(dim=1))
+        #self.conv3 = 
+        self.conv4 = nn.Conv2d(384, 128, kernel_size=3,dilation=2) # change back to square filter and dilation 2 for digit
+        self.conv5 = nn.Conv2d(128, 64, kernel_size=3,dilation=2)
+        self.max3 = nn.MaxPool2d(kernel_size=3,stride=2)
     def forward(self, input_data, alpha):
+        #input_data = input_data.expand(input_data.data.shape[0], 3, 32, 32)
         feature = self.features(input_data)
+        #feature = self.feature(feature)
         
         feature = self.relu(self.bn1(self.conv4(feature)))
         feature = self.bottleneck2(feature)
+        #feature = feature_o1 + feature_n1
         feature = self.cbam1(feature)
         feature = self.relu(self.bn2(self.conv5(feature)))
         feature = self.bottleneck4(feature)
+        #feature = feature_o2 + feature_n2
         feature = self.cbam2(feature)
         feature = self.max3(feature)
         
-        # Print the shape of the tensor before view operation
-        print("Shape before view:", feature.shape)
-        
-        feature = feature.view(-1, 128 * 6 * 6)  # Flatten the tensor appropriately
-        reverse_feature = ReverseLayerF.apply(feature, alpha)
+
+        feature = feature.view(-1, 256 * 4 * 4) #256 for digit
+        reverse_feature = ReverseLayerF.apply(feature, 1.0)
         class_output = self.class_classifier(feature)
         domain_output = self.domain_classifier(reverse_feature)
-
+        #print(class_output.shape)
         return class_output, domain_output
