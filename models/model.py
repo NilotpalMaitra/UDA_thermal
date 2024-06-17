6346ed5d9dbdc49a01d6d9920eb8590df29a19fb
Nilvalox <nilotpalm2015@gmail.com>
2024-06-17 15:53:54 +0530

    commit

--- models/model.py
+++ models/model.py
@@ -2,7 +2,6 @@ import torch.nn as nn
 from models.functions import ReverseLayerF
 import torch
 from torchvision import models
-import math
 import torch.nn.functional as F
 
 class BasicConv(nn.Module):
@@ -10,7 +9,7 @@ class BasicConv(nn.Module):
         super(BasicConv, self).__init__()
         self.out_channels = out_planes
         self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
-        self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.bn = nn.BatchNorm2d(out_planes, eps=1e-5, momentum=0.01, affine=True) if bn else None
         self.relu = nn.ReLU() if relu else None
 
     def forward(self, x):
@@ -34,31 +33,31 @@ class ChannelGate(nn.Module):
             nn.Linear(gate_channels, gate_channels // reduction_ratio),
             nn.ReLU(),
             nn.Linear(gate_channels // reduction_ratio, gate_channels)
-            )
+        )
         self.pool_types = pool_types
+
     def forward(self, x):
         channel_att_sum = None
         for pool_type in self.pool_types:
-            if pool_type=='avg':
-                avg_pool = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-                channel_att_raw = self.mlp( avg_pool )
-            elif pool_type=='max':
-                max_pool = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-                channel_att_raw = self.mlp( max_pool )
-            elif pool_type=='lp':
-                lp_pool = F.lp_pool2d( x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-                channel_att_raw = self.mlp( lp_pool )
-            elif pool_type=='lse':
-                # LSE pool only
+            if pool_type == 'avg':
+                avg_pool = F.avg_pool2d(x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp(avg_pool)
+            elif pool_type == 'max':
+                max_pool = F.max_pool2d(x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp(max_pool)
+            elif pool_type == 'lp':
+                lp_pool = F.lp_pool2d(x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+                channel_att_raw = self.mlp(lp_pool)
+            elif pool_type == 'lse':
                 lse_pool = logsumexp_2d(x)
-                channel_att_raw = self.mlp( lse_pool )
+                channel_att_raw = self.mlp(lse_pool)
 
             if channel_att_sum is None:
                 channel_att_sum = channel_att_raw
             else:
-                channel_att_sum = channel_att_sum + channel_att_raw
+                channel_att_sum += channel_att_raw
 
-        scale = F.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
+        scale = torch.sigmoid(channel_att_sum).unsqueeze(2).unsqueeze(3).expand_as(x)
         return x * scale
 
 def logsumexp_2d(tensor):
@@ -69,35 +68,34 @@ def logsumexp_2d(tensor):
 
 class ChannelPool(nn.Module):
     def forward(self, x):
-        return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1)), dim=1 )
+        return torch.cat((torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)), dim=1)
 
 class SpatialGate(nn.Module):
     def __init__(self):
         super(SpatialGate, self).__init__()
         kernel_size = 7
         self.compress = ChannelPool()
-        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size-1) // 2, relu=False)
+        self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size - 1) // 2, relu=False)
+
     def forward(self, x):
         x_compress = self.compress(x)
         x_out = self.spatial(x_compress)
-        scale = F.sigmoid(x_out) # broadcasting
+        scale = torch.sigmoid(x_out)
         return x * scale
 
 class CBAM(nn.Module):
     def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'lse'], no_spatial=True):
         super(CBAM, self).__init__()
         self.ChannelGate = ChannelGate(gate_channels, reduction_ratio, pool_types)
-        self.no_spatial=no_spatial
-        #if not no_spatial:
-            #self.SpatialGate = SpatialGate()
+        self.no_spatial = no_spatial
+        if not no_spatial:
+            self.SpatialGate = SpatialGate()
+
     def forward(self, x):
         x_out = self.ChannelGate(x)
-        #if not self.no_spatial:
-            #x_out = self.SpatialGate(x_out)
+        if not self.no_spatial:
+            x_out = self.SpatialGate(x_out)
         return x_out
-
-
-
 
 class CNNModel(nn.Module):
 
@@ -106,8 +104,8 @@ class CNNModel(nn.Module):
         
         pre_a = models.alexnet(pretrained=True)
         self.features = pre_a.features[0:8]
-        for i, p in enumerate(self.features.parameters()):
-            p.requires_grad = False
+        for param in self.features.parameters():
+            param.requires_grad = False
 
         self.bn1 = nn.BatchNorm2d(128)
         self.bn2 = nn.BatchNorm2d(64)
@@ -141,7 +139,7 @@ class CNNModel(nn.Module):
         )
 
         self.class_classifier = nn.Sequential(
-            nn.Linear(256 * 6 * 6, 100),  # Adjust the input size according to your input image size
+            nn.Linear(128 * 6 * 6, 100),  # Adjusted input size
             nn.BatchNorm1d(100),
             nn.ReLU(True),
             nn.Linear(100, 100),
@@ -152,14 +150,14 @@ class CNNModel(nn.Module):
         )
 
         self.domain_classifier = nn.Sequential(
-            nn.Linear(256 * 6 * 6, 100),
+            nn.Linear(128 * 6 * 6, 100),  # Adjusted input size
             nn.BatchNorm1d(100),
             nn.ReLU(True),
             nn.Linear(100, 2),
             nn.LogSoftmax(dim=1)
         )
 
-        self.conv4 = nn.Conv2d(384, 128, kernel_size=3, dilation=2)
+        self.conv4 = nn.Conv2d(256, 128, kernel_size=3, dilation=2)
         self.conv5 = nn.Conv2d(128, 64, kernel_size=3, dilation=2)
         self.max3 = nn.MaxPool2d(kernel_size=3, stride=2)
 
@@ -173,9 +171,12 @@ class CNNModel(nn.Module):
         feature = self.bottleneck4(feature)
         feature = self.cbam2(feature)
         feature = self.max3(feature)
-
-        feature = feature.view(-1, 256 * 6 * 6)  # Flatten the tensor appropriately
-        reverse_feature = ReverseLayerF.apply(feature, 1.0)
+        
+        # Print the shape of the tensor before view operation
+        print("Shape before view:", feature.shape)
+        
+        feature = feature.view(feature.size(0), -1)  # Flatten the tensor appropriately
+        reverse_feature = ReverseLayerF.apply(feature, alpha)
         class_output = self.class_classifier(feature)
         domain_output = self.domain_classifier(reverse_feature)
 
