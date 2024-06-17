ea54365db09adb518aa781ac218944e5df8378b7
Nilvalox <nilotpalm2015@gmail.com>
2024-06-17 15:12:46 +0530

    commit

--- models/model.py
+++ models/model.py
@@ -104,89 +104,79 @@ class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
         
-        pre_a =  models.alexnet(pretrained=True)
+        pre_a = models.alexnet(pretrained=True)
         self.features = pre_a.features[0:8]
-        for i,p in enumerate(self.features.parameters()):
-            #if(i<6): #[changed to [2,4,6] depending on how many conv layers to freeze], 132 for mobilenet
+        for i, p in enumerate(self.features.parameters()):
             p.requires_grad = False
 
-        
-        self.bn1 = nn.BatchNorm2d(128) #128 for digit 256 for alpha
-        self.bn2 = nn.BatchNorm2d(64) # 64 for digit 128 for alpha
+        self.bn1 = nn.BatchNorm2d(128)
+        self.bn2 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(True)
 
-        self.cbam1 = CBAM(128) #128 for digit 256 for alpha
-        self.cbam2 = CBAM(64) # 64 for digit 128 for alpha
+        self.cbam1 = CBAM(128)
+        self.cbam2 = CBAM(64)
 
-        self.bottleneck2 = nn.Sequential()
-        self.bottleneck2.add_module('b2_conv1',nn.Conv2d(128, 32, kernel_size=1,stride=1,padding = 'same')) # change with 128 and 32 for digit, 256 and 64 for alpha
-        #self.bottleneck2.add_module('b2_r1',nn.ReLU(True))
-        self.bottleneck2.add_module('b2_bn1',nn.BatchNorm2d(32))
-        self.bottleneck2.add_module('b2_r1',nn.ReLU(True))
-        self.bottleneck2.add_module('b2_conv2',nn.Conv2d(32, 32, kernel_size=3,stride=1,padding = 'same',dilation=2))
-        #self.bottleneck2.add_module('b2_r2',nn.ReLU(True))
-        self.bottleneck2.add_module('b2_bn2',nn.BatchNorm2d(32))
-        self.bottleneck2.add_module('b2_r2',nn.ReLU(True))
-        self.bottleneck2.add_module('b2_conv3',nn.Conv2d(32, 128, kernel_size=1,stride=1,padding = 'same'))
-        #self.bottleneck2.add_module('b2_r3',nn.ReLU(True))
-        self.bottleneck2.add_module('b2_bn3',nn.BatchNorm2d(128))
-        self.bottleneck2.add_module('b2_r3',nn.ReLU(True))
-        
-        self.bottleneck4 = nn.Sequential()
-        self.bottleneck4.add_module('b4_conv1',nn.Conv2d(64, 16, kernel_size=1,stride=1,padding = 'same')) # change with 64 and 16 for digit, 128 and 32 for alpha
-        #self.bottleneck4.add_module('b4_r1',nn.ReLU(True))
-        self.bottleneck4.add_module('b4_bn1',nn.BatchNorm2d(16))
-        self.bottleneck4.add_module('b4_r1',nn.ReLU(True))
-        self.bottleneck4.add_module('b4_conv2',nn.Conv2d(16, 16, kernel_size=3,stride=1,padding = 'same', dilation=2))
-        #self.bottleneck4.add_module('b4_r2',nn.ReLU(True))
-        self.bottleneck4.add_module('b4_bn2',nn.BatchNorm2d(16))
-        self.bottleneck4.add_module('b4_r2',nn.ReLU(True))
-        self.bottleneck4.add_module('b4_conv3',nn.Conv2d(16, 64, kernel_size=1,stride=1,padding = 'same'))
-        #self.bottleneck4.add_module('b4_r3',nn.ReLU(True))
-        self.bottleneck4.add_module('b4_bn3',nn.BatchNorm2d(64))
-        self.bottleneck4.add_module('b4_r3',nn.ReLU(True))
-        #'''
-        
-        self.class_classifier = nn.Sequential()
-        self.class_classifier.add_module('c_fc1', nn.Linear(256, 100))  #256 for digit 128*4*4 for aplha
-        self.class_classifier.add_module('c_bn1', nn.BatchNorm1d(100)) # 500 for alpha
-        self.class_classifier.add_module('c_relu1', nn.ReLU(True))
-        #self.class_classifier.add_module('c_drop1', nn.Dropout2d())
-        self.class_classifier.add_module('c_fc2', nn.Linear(100, 100))
-        self.class_classifier.add_module('c_bn2', nn.BatchNorm1d(100))
-        self.class_classifier.add_module('c_relu2', nn.ReLU(True))
-        self.class_classifier.add_module('c_fc3', nn.Linear(100, 10)) #10 for digit and 4 for alpha
-        self.class_classifier.add_module('c_softmax', nn.LogSoftmax())
+        self.bottleneck2 = nn.Sequential(
+            nn.Conv2d(128, 32, kernel_size=1, stride=1, padding='same'),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding='same', dilation=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.Conv2d(32, 128, kernel_size=1, stride=1, padding='same'),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True)
+        )
 
-        self.domain_classifier = nn.Sequential()
-        self.domain_classifier.add_module('d_fc1', nn.Linear(128*4*4, 100))#256 for digit 128*4*4 for aplha
-        self.domain_classifier.add_module('d_bn1', nn.BatchNorm1d(100))
-        self.domain_classifier.add_module('d_relu1', nn.ReLU(True))
-        self.domain_classifier.add_module('d_fc2', nn.Linear(100, 2))
-        self.domain_classifier.add_module('d_softmax', nn.LogSoftmax(dim=1))
-        #self.conv3 = 
-        self.conv4 = nn.Conv2d(384, 128, kernel_size=3,dilation=2) # change back to square filter and dilation 2 for digit
-        self.conv5 = nn.Conv2d(128, 64, kernel_size=3,dilation=2)
-        self.max3 = nn.MaxPool2d(kernel_size=3,stride=2)
+        self.bottleneck4 = nn.Sequential(
+            nn.Conv2d(64, 16, kernel_size=1, stride=1, padding='same'),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding='same', dilation=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.Conv2d(16, 64, kernel_size=1, stride=1, padding='same'),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True)
+        )
+
+        self.class_classifier = nn.Sequential(
+            nn.Linear(256 * 6 * 6, 100),  # Adjust the input size according to your input image size
+            nn.BatchNorm1d(100),
+            nn.ReLU(True),
+            nn.Linear(100, 100),
+            nn.BatchNorm1d(100),
+            nn.ReLU(True),
+            nn.Linear(100, 10),
+            nn.LogSoftmax(dim=1)
+        )
+
+        self.domain_classifier = nn.Sequential(
+            nn.Linear(256 * 6 * 6, 100),
+            nn.BatchNorm1d(100),
+            nn.ReLU(True),
+            nn.Linear(100, 2),
+            nn.LogSoftmax(dim=1)
+        )
+
+        self.conv4 = nn.Conv2d(384, 128, kernel_size=3, dilation=2)
+        self.conv5 = nn.Conv2d(128, 64, kernel_size=3, dilation=2)
+        self.max3 = nn.MaxPool2d(kernel_size=3, stride=2)
+
     def forward(self, input_data, alpha):
-        #input_data = input_data.expand(input_data.data.shape[0], 3, 32, 32)
         feature = self.features(input_data)
-        #feature = self.feature(feature)
         
         feature = self.relu(self.bn1(self.conv4(feature)))
         feature = self.bottleneck2(feature)
-        #feature = feature_o1 + feature_n1
         feature = self.cbam1(feature)
         feature = self.relu(self.bn2(self.conv5(feature)))
         feature = self.bottleneck4(feature)
-        #feature = feature_o2 + feature_n2
         feature = self.cbam2(feature)
         feature = self.max3(feature)
-        
 
-        feature = feature.view(-1, 256) #256 for digit
+        feature = feature.view(-1, 256 * 6 * 6)  # Flatten the tensor appropriately
         reverse_feature = ReverseLayerF.apply(feature, 1.0)
         class_output = self.class_classifier(feature)
         domain_output = self.domain_classifier(reverse_feature)
-        #print(class_output.shape)
+
         return class_output, domain_output
