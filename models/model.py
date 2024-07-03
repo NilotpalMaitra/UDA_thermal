b746ffcab0fed11f0696f82a8c5a2f1036d17355
Nilvalox <nilotpalm2015@gmail.com>
2024-07-03 21:06:42 +0530

    maybe fixed

--- models/model.py
+++ models/model.py
@@ -103,14 +103,14 @@ class CNNModel(nn.Module):
 
     def __init__(self):
         super(CNNModel, self).__init__()
-        
+
         pre_a =  models.alexnet(pretrained=True)
         self.features = pre_a.features[0:8]
         for i,p in enumerate(self.features.parameters()):
             #if(i<6): #[changed to [2,4,6] depending on how many conv layers to freeze], 132 for mobilenet
             p.requires_grad = False
 
-        
+
         self.bn1 = nn.BatchNorm2d(128) #128 for digit 256 for alpha
         self.bn2 = nn.BatchNorm2d(64) # 64 for digit 128 for alpha
         self.relu = nn.ReLU(True)
@@ -131,7 +131,7 @@ class CNNModel(nn.Module):
         #self.bottleneck2.add_module('b2_r3',nn.ReLU(True))
         self.bottleneck2.add_module('b2_bn3',nn.BatchNorm2d(128))
         self.bottleneck2.add_module('b2_r3',nn.ReLU(True))
-        
+
         self.bottleneck4 = nn.Sequential()
         self.bottleneck4.add_module('b4_conv1',nn.Conv2d(64, 16, kernel_size=1,stride=1,padding = 'same')) # change with 64 and 16 for digit, 128 and 32 for alpha
         #self.bottleneck4.add_module('b4_r1',nn.ReLU(True))
@@ -146,7 +146,7 @@ class CNNModel(nn.Module):
         self.bottleneck4.add_module('b4_bn3',nn.BatchNorm2d(64))
         self.bottleneck4.add_module('b4_r3',nn.ReLU(True))
         #'''
-        
+
         self.class_classifier = nn.Sequential()
         self.class_classifier.add_module('c_fc1', nn.Linear(256, 100))  #256 for digit 128*4*4 for aplha
         self.class_classifier.add_module('c_bn1', nn.BatchNorm1d(100)) # 500 for alpha
@@ -159,20 +159,20 @@ class CNNModel(nn.Module):
         self.class_classifier.add_module('c_softmax', nn.LogSoftmax())
 
         self.domain_classifier = nn.Sequential()
-        self.domain_classifier.add_module('d_fc1', nn.Linear(128*4*4, 100))#256 for digit 128*4*4 for aplha
+        self.domain_classifier.add_module('d_fc1', nn.Linear(256, 100))#256 for digit 128*4*4 for aplha
         self.domain_classifier.add_module('d_bn1', nn.BatchNorm1d(100))
         self.domain_classifier.add_module('d_relu1', nn.ReLU(True))
         self.domain_classifier.add_module('d_fc2', nn.Linear(100, 2))
         self.domain_classifier.add_module('d_softmax', nn.LogSoftmax(dim=1))
-        #self.conv3 = 
-        self.conv4 = nn.Conv2d(256, 128, kernel_size=3,dilation=2) # change back to square filter and dilation 2 for digit
+        #self.conv3 =
+        self.conv4 = nn.Conv2d(384, 128, kernel_size=3,dilation=2) # change back to square filter and dilation 2 for digit
         self.conv5 = nn.Conv2d(128, 64, kernel_size=3,dilation=2)
         self.max3 = nn.MaxPool2d(kernel_size=3,stride=2)
-    def forward(self, input_data, alpha):
+    def forward(self, input_data, alpha=1.0):
         #input_data = input_data.expand(input_data.data.shape[0], 3, 32, 32)
         feature = self.features(input_data)
         #feature = self.feature(feature)
-        
+
         feature = self.relu(self.bn1(self.conv4(feature)))
         feature = self.bottleneck2(feature)
         #feature = feature_o1 + feature_n1
@@ -182,9 +182,9 @@ class CNNModel(nn.Module):
         #feature = feature_o2 + feature_n2
         feature = self.cbam2(feature)
         feature = self.max3(feature)
-        
 
-        feature = feature.view(-1, 256*4*4) #256 for digit
+
+        feature = feature.view(-1, 256) #256 for digit
         reverse_feature = ReverseLayerF.apply(feature, 1.0)
         class_output = self.class_classifier(feature)
         domain_output = self.domain_classifier(reverse_feature)
